abf7aa205feccf76ae4b0c51d5d0fa55cbdac525
somin-jeong <81250561+somin-jeong@users.noreply.github.com>
2023-06-28 16:44:08 +0900

    Chore: project setting

---
+++ src/main/java/com/kuit/conet/ConetApplication.java
@@ -0,0 +1,14 @@
+package com.kuit.conet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConetApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/kuit/conet/ConetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuit.conet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
