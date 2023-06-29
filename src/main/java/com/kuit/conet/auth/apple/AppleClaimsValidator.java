0394d088c2965d41e35eab4ab623a56ee0e545a1
ro-el-c <chung3286@naver.com>
2023-06-30 01:04:28 +0900

    fix: nonce 주석 처리
    Client로부터 받은 id token에 nonce 필드 부재로 nonce 검증 주석 처리

--- src/main/java/com/kuit/conet/auth/apple/AppleClaimsValidator.java
+++ src/main/java/com/kuit/conet/auth/apple/AppleClaimsValidator.java
@@ -1,6 +1,5 @@
 package com.kuit.conet.auth.apple;
 
-import com.kuit.conet.auth.util.EncryptUtils;
 import io.jsonwebtoken.Claims;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -11,26 +10,27 @@ import org.springframework.stereotype.Component;
 @Getter
 @Component
 public class AppleClaimsValidator {
-    private static final String NONCE_KEY = "nonce";
+    // 클라이언트로부터 전달 받은 Apple 의 id_token 이 nonce 필드를 포함하지 않음
+    //private static final String NONCE_KEY = "nonce";
 
     private final String iss;
     private final String clientId;
-    private final String nonce;
+    //private final String nonce;
 
     public AppleClaimsValidator (
             @Value("${oauth.apple.iss}") String iss,
-            @Value("${oauth.apple.client-id}") String clientId,
-            @Value("${oauth.apple.nonce}") String nonce
+            @Value("${oauth.apple.client-id}") String clientId
+            //@Value("${oauth.apple.nonce}") String nonce
     ) {
         this.iss = iss;
         this.clientId = clientId;
-        this.nonce = EncryptUtils.encrypt(nonce);
+        //this.nonce = EncryptUtils.encrypt(nonce);
     }
 
     public boolean isValid(Claims claims) {
         return claims.getIssuer().contains(iss) &&
-                claims.getAudience().equals(clientId) &&
-                EncryptUtils.encrypt(claims.get(NONCE_KEY, String.class)).equals(nonce);
+                claims.getAudience().equals(clientId); // &&
+                //claims.get(NONCE_KEY, String.class).equals(nonce);
     }
 }
 
