dd2914d3d7015951ca7f481b0cf896cce47fece3
ro-el-c <chung3286@naver.com>
2023-06-29 21:32:09 +0900

    style: 공백 추가

--- src/main/java/com/kuit/conet/common/exception/InvalidTokenException.java
+++ src/main/java/com/kuit/conet/common/exception/InvalidTokenException.java
@@ -4,7 +4,7 @@ import com.kuit.conet.common.response.status.ResponseStatus;
 
 import static com.kuit.conet.common.response.status.BaseExceptionResponseStatus.UNSUPPORTED_TOKEN_TYPE_FOR_APPLE;
 
-public class InvalidTokenException extends RuntimeException{
+public class InvalidTokenException extends RuntimeException {
     private final ResponseStatus exceptionStatus;
 
     public InvalidTokenException(ResponseStatus responseStatus) {
