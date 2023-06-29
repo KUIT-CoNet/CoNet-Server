ca5e6a4afc58bb89df5906714f3cac00b64391c9
ro-el-c <chung3286@naver.com>
2023-06-29 14:04:01 +0900

    feat: Invalid platform exception 처리
    옳지 못한 플랫폼을 입력할 때 발생하는 오류에 대한 exception

---
+++ src/main/java/com/kuit/conet/common/exception/PlatformException.java
@@ -0,0 +1,15 @@
+package com.kuit.conet.common.exception;
+
+import com.kuit.conet.common.response.status.ResponseStatus;
+
+import static com.kuit.conet.common.response.status.BaseExceptionResponseStatus.INVALID_PLATFORM;
+
+public class PlatformException extends RuntimeException {
+
+    private final ResponseStatus exceptionStatus;
+
+    public PlatformException() {
+        super(INVALID_PLATFORM.getMessage());
+        this.exceptionStatus = INVALID_PLATFORM;
+    }
+}리
--- src/main/java/com/kuit/conet/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/kuit/conet/common/response/status/BaseExceptionResponseStatus.java
@@ -33,8 +33,12 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
     INVALID_TOKEN(4003, HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 토큰입니다."),
     MALFORMED_TOKEN(4004, HttpStatus.UNAUTHORIZED.value(), "토큰이 올바르게 구성되지 않았습니다."),
     EXPIRED_TOKEN(4005, HttpStatus.UNAUTHORIZED.value(), "만료된 토큰입니다."),
-    TOKEN_MISMATCH(4006, HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 토큰 정보와 일치하지 않습니다.");
+    TOKEN_MISMATCH(4006, HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 토큰 정보와 일치하지 않습니다."),
 
+    /**
+     * 5000: 회원 정보 오류
+     * */
+    INVALID_PLATFORM(5001, HttpStatus.BAD_REQUEST.value(),"플랫폼 정보가 올바르지 않습니다.");
 
     private final int code;
     private final int status;
