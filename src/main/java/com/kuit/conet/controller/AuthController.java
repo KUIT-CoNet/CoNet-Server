2875e026ae4b7897b3daaaaa6f5236279c90e596
ro-el-c <chung3286@naver.com>
2023-06-29 13:57:30 +0900

    add: add AuthController, AuthService

---
+++ src/main/java/com/kuit/conet/controller/AuthController.java
@@ -0,0 +1,7 @@
+package com.kuit.conet.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
---
+++ src/main/java/com/kuit/conet/service/AuthService.java
@@ -0,0 +1,5 @@
+package com.kuit.conet.service;
+
+public class AuthService {
+    // Todo: AuthService 회원 존재 여부에 따라 다른 Response 형식 반환 구현
+}
