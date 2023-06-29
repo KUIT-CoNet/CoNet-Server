f7565466bfdb15ffdeefd2b41fa4191a079db571
ro-el-c <chung3286@naver.com>
2023-06-30 03:53:13 +0900

    fix: LoginResponse 변경에 따른 Service 수정

--- src/main/java/com/kuit/conet/service/AuthService.java
+++ src/main/java/com/kuit/conet/service/AuthService.java
@@ -8,7 +8,7 @@ import com.kuit.conet.domain.Platform;
 import com.kuit.conet.domain.User;
 import com.kuit.conet.dto.request.AppleLoginRequest;
 import com.kuit.conet.dto.response.ApplePlatformUserResponse;
-import com.kuit.conet.dto.response.OAuthTokenResponse;
+import com.kuit.conet.dto.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,31 +17,33 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    // Todo: AuthService 회원 존재 여부에 따라 다른 Response 형식 반환 구현
     private final UserDao userDao;
     private final AppleOAuthUserProvider appleOAuthUserProvider;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public OAuthTokenResponse appleOAuthLogin(AppleLoginRequest loginRequest) {
+    public LoginResponse appleOAuthLogin(AppleLoginRequest loginRequest) {
         ApplePlatformUserResponse applePlatformUser = appleOAuthUserProvider.getApplePlatformUser(loginRequest.getToken());
         return generateOAuthTokenResponse(Platform.APPLE, applePlatformUser.getEmail(), applePlatformUser.getPlatformId());
     }
 
-    private OAuthTokenResponse generateOAuthTokenResponse(Platform platform, String email, String platformId) {
+    private LoginResponse generateOAuthTokenResponse(Platform platform, String email, String platformId) {
         return userDao.findByPlatformAndPlatformId(platform, platformId)
                 .map(userId -> {
                     User findUser = userDao.findById(userId).orElseThrow(NotFoundUserException::new);
-                    String token = issueToken(findUser);
-                    return new OAuthTokenResponse(token, findUser.getEmail(), true, platformId);
+                    return getLoginResponse(findUser);
                 })
                 .orElseGet(() -> {
                     User oauthUser = new User(email, platform, platformId);
-                    User saveduser = userDao.save(oauthUser);
-                    String token = issueToken(saveduser);
-                    return new OAuthTokenResponse(token, email, false, platformId);
+                    User saveduser = userDao.save(oauthUser).get();
+                    return getLoginResponse(saveduser);
                 });
     }
-    private String issueToken(final User findUser) {
-        return jwtTokenProvider.createToken(findUser.getUserId());
+
+    private LoginResponse getLoginResponse(User targetUser) {
+        String accessToken = jwtTokenProvider.createAccessToken(targetUser.getUserId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(targetUser.getUserId());
+        //TODO: Redis 에 refresh token 저장
+
+        return new LoginResponse(accessToken, refreshToken, targetUser.getEmail());
     }
 }
