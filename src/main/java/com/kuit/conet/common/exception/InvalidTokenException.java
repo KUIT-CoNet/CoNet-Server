df2a200361ad5ae7ca176d42cee6700c411a8ea5
ro-el-c <chung3286@naver.com>
2023-07-02 21:06:29 +0900

    style: 불필요한 import 삭제

--- src/main/java/com/kuit/conet/common/exception/InvalidTokenException.java
+++ src/main/java/com/kuit/conet/common/exception/InvalidTokenException.java
@@ -2,8 +2,6 @@ package com.kuit.conet.common.exception;
 
 import com.kuit.conet.common.response.status.ResponseStatus;
 
-import static com.kuit.conet.common.response.status.BaseExceptionResponseStatus.UNSUPPORTED_TOKEN_TYPE_FOR_APPLE;
-
 public class InvalidTokenException extends RuntimeException {
     private final ResponseStatus exceptionStatus;
 
